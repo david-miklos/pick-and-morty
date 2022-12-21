cbf3e98f7d3dfc52db1fcfb899afd0733d75d673
david-miklos <david.miklos@siemens.com>
2022-12-21 10:12:31 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import FilterBox from "./FilterBox";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Header";
+import Favourites from "./Favourites";
 
 function App() {
   return (
     <div>
-      <Header />
-      <FilterBox />
+      {/* <Header />
+      <FilterBox /> */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/favourites" element={<Favourites />}></Route>
+          <Route path="/" element={<FilterBox />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div className="header border-b border-gray-200 h-16">
-      <h1 className="flex flex-col items-center h-full justify-center text-3xl font-bold text-gray-800">
+    <div className="header border-b flex flex-row justify-center border-gray-200 h-16">
+      <Link
+        to="/"
+        className="flex h-full items-center ml-auto text-4xl font-extrabold text-gray-800"
+      >
         Pick and Morty
-      </h1>
+      </Link>
+      <div className="flex items-centre ml-auto">
+        <Link
+          to="/favourites"
+          className="liked-button flex items-center h-2/3 my-auto mr-10"
+        >
+          FAVOURITES
+        </Link>
+      </div>
     </div>
   );
 };
