46788fd1379149f29f8ba3fdad3978ed3dba1ca1
david-miklos <david.miklos@siemens.com>
2023-01-04 17:51:09 +0200

    add no favourite characters message

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -6,7 +6,15 @@ const Favourites = () => {
   const favouriteCharacters = useSelector(
     (state: RootState) => state.favouriteCharacters.value
   );
-
+  if (!favouriteCharacters.length) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen font-bold text-lg xl:text-3xl lg:text-2xl md:text-xl text-gray-800">
+        <div className="bg-gray-200 p-10 rounded-lg">
+          No characters in favourites.
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <ul className="favourites mx-auto my-12 grid gap-5 w-11/12 2xl:grid-cols-5 2xl:col-span-5 xl:grid-cols-4 xl:col-span-4 lg:grid-cols-3 lg:col-span-3 md:grid-cols-2 md:col-span-2 sm:grid-cols-1 sm:col-span-1">
