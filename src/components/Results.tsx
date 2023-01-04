65fa61d4e92ad420cb610b89a61455ba1e627479
david-miklos <david.miklos@siemens.com>
2023-01-04 17:36:43 +0200

    update results to list

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -7,11 +7,15 @@ const Results = ({ results }: { results: ICharacter[] }) => {
     return <NotFound />;
   }
   return (
-    <div className="results grid gap-5 2xl:grid-cols-4 2xl:col-span-4 xl:grid-cols-3 xl:col-span-3 lg:grid-cols-2 lg:col-span-2 md:grid-cols-2 md:col-span-2 sm:grid-cols-1 sm:col-span-1">
+    <ul className="results grid gap-5 2xl:grid-cols-4 2xl:col-span-4 xl:grid-cols-3 xl:col-span-3 lg:grid-cols-2 lg:col-span-2 md:grid-cols-2 md:col-span-2 sm:grid-cols-1 sm:col-span-1">
       {results.map((character) => {
-        return <Character {...character} key={character.id} />;
+        return (
+          <li key={character.id}>
+            <Character {...character} />
+          </li>
+        );
       })}
-    </div>
+    </ul>
   );
 };
 
